96b3825d65cb787c56ace31383a845eab85d6dda
jhnl18 <jhunelpenaflorida@gmail.com>
2025-12-05 13:10:50 +0800

    Initial commit

---
+++ navigation/StackNavigator.js
@@ -0,0 +1,14 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import DetailsScreen from '../screens/DetailsScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function StackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  );
+}
